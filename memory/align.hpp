7e06574091da3d0595bef818185990a91aeffa8d
ppatoria <ppatoria@gmail.com>
2023-12-04 21:12:24 -0500

    dev

--- memory/align.hpp
+++ memory/align.hpp
@@ -4,7 +4,7 @@ namespace quick {
 namespace memory {
 
 #ifndef QUICK_CACHE_LINE_SIZE
-#define QUICK_CACHE_LINE_SIZE 64
+    #define QUICK_CACHE_LINE_SIZE 64
 #endif
 
 #define QUICK_CACHE_LINE_ALIGNED alignas(QUICK_CACHE_LINE_SIZE)
--- tests/thread.cpp
+++ tests/thread.cpp
@@ -1,7 +1,10 @@
 #include "catch2/catch_all.hpp"
+#include "quick/phoenix.hpp"
 #include "quick/thread/spin_lock.hpp"
+#include <cstdint>
 #include <string>
 
 TEST_CASE("thread", "[misc tests]")
-{
+{    
+    quick::thread::spin_lock lock;
 }
